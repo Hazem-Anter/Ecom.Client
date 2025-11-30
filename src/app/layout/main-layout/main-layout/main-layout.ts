1d0dbf97d1e079fe54672f1f9cff416357a1b462
FadyTawadrous <40478872+FadyTawadrous@users.noreply.github.com>
2025-11-30 17:26:34 +0200

    added nav bar to test authentication flow, added logout, recaftor user image handling to have it in auth service

--- src/app/layout/main-layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout/main-layout.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TestNavbar } from '../../../shared/components/test-navbar/test-navbar';
 
 // You will create these components later
 // import { NavbarComponent } from '../navbar/navbar.component';
@@ -8,7 +9,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, TestNavbar],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
